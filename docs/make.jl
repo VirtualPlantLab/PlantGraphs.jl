255487ced414f004df6df6fb81b762e835b52c88
Alejandro Morales Sierra <alejandro.moralessierra@wur.nl>
2023-09-18 07:55:33 +0200

    Build documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,23 @@
 using PlantGraphs
 using Documenter
-using DocumenterMarkdown
 
 DocMeta.setdocmeta!(PlantGraphs, :DocTestSetup, :(using PlantGraphs); recursive=true)
 
 makedocs(;
          modules = [PlantGraphs],
-         format = Markdown(),
+         format = Documenter.HTML(;
+            prettyurls = get(ENV, "CI", "false") == "true",
+            edit_link = "master",
+            assets = String[],
+         ),
          authors = "Alejandro Morales Sierra <alejandro.moralessierra@wur.nl> and contributors",
          repo = "https://github.com/VirtualPlantLab/PlantGraphs.jl/blob/{commit}{path}#{line}",
          sitename = "PlantGraphs.jl",
          pages = [
              "Home" => "index.md",
          ])
+
+deploydocs(;
+         repo="github.com/VirtualPlantLab/PlantGraphs.jl",
+         devbranch="master"
+)
